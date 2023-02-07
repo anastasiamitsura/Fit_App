2b27664e29b86ad6cb2577cec6ae958b5205019b
Nastya_Mits <nastamicura2@gmail.com>
2023-02-07 20:08:44 +0700

    Interface commit

--- app/src/main/java/com/example/fit_app/MainActivity.java
+++ app/src/main/java/com/example/fit_app/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.fit_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.fit_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homebt, R.id.settingbt, R.id.profilebt)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navV, navController);
     }
 }
